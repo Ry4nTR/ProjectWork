02b3410e57cb6aaef2287d76a31e8d2e711ac37f
Ry4nTR <rostukterletskyy@gmail.com>
2025-04-12 00:09:44 +0200

    Scheletro interazioni Gamescene

---
+++ Assets/_Scripts/Interaction/Interactable Objects/Letto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using ITSProjectWork;
+using UnityEngine;
+
+public class Letto : MonoBehaviour, IInteractable
+{
+
+    public void Interact()
+    {
+        BlackScreenTextController.Instance.ActivateBlackScreen("TUA MADRE DORME");
+    }
+
+
+}
+
