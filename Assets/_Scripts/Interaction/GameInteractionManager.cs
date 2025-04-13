8e93d945a376f69e648a1fb6c77d861fcbb5c6bf
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2025-04-13 18:49:14 +0200

    Created logic for day cycle + finishing + reordering code

--- Assets/_Scripts/Interaction/GameInteractionManager.cs
+++ Assets/_Scripts/Interaction/GameInteractionManager.cs
@@ -8,8 +8,12 @@ namespace ProjectWork
     {
         public static GameInteractionManager Instance { get; private set; }
 
+        public static event Action OnTutorialFinished = delegate { };
+
         [SerializeField] private CheckListManager<InteractableObject> listCheckManager;
         [SerializeField] private Bed bedInteraction;
+        [SerializeField] private int maxDays = 3;
+        private int currentDay = 1;
 
         private void Awake()
         {
@@ -20,13 +24,18 @@ namespace ProjectWork
 
                 TrashManager.OnTrashSpawned += AddToListAndSubscribeToTrashThrownEvent;
                 bedInteraction.OnInteractionFinished += ResetInteractions;
-                listCheckManager.OnListCompleted += bedInteraction.UnlockInteraction;
+                listCheckManager.OnListCompleted += UnlockBedInteraction;
             }
             else
             {
                 Destroy(gameObject);
             }   
-        }        
+        }
+
+        private void Start()
+        {
+            currentDay = 1;
+        }
 
         private void OnDestroy()
         {
@@ -37,18 +46,29 @@ namespace ProjectWork
 
             TrashManager.OnTrashSpawned -= AddToListAndSubscribeToTrashThrownEvent;
             bedInteraction.OnInteractionFinished -= ResetInteractions;
-            listCheckManager.OnListCompleted -= bedInteraction.UnlockInteraction;
+            listCheckManager.OnListCompleted -= UnlockBedInteraction;
+        }
+
+        private void UnlockBedInteraction()
+        {
+            currentDay++;
+            if(currentDay > maxDays)
+            {
+                OnTutorialFinished?.Invoke();
+            }
+            else
+            {
+                bedInteraction.UnlockInteraction();
+            }    
         }
 
         private void AddToListAndSubscribeToTrashThrownEvent(Trash spawnedTrash)
         {
             listCheckManager.AddItemToCheckList(spawnedTrash);
-            //spawnedTrash.OnTrashThrown += ;
         }
         
         private void ResetInteractions(InteractableObject eventInvoker)
-        {
-            
+        {          
             foreach (CheckListManager<InteractableObject>.ItemCheck item in listCheckManager.Items)
             {
                 item.element.ResetInteraction();
