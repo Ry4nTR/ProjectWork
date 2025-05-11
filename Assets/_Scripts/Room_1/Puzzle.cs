134b15e93de2b8a7f53191d00839bb6b549b41c1
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2025-05-11 15:06:39 +0200

    Added summaries for PuzzleCompleted events

--- Assets/_Scripts/Room_1/Puzzle.cs
+++ Assets/_Scripts/Room_1/Puzzle.cs
@@ -5,7 +5,14 @@ namespace ProjectWork
 {
     public abstract class Puzzle : MonoBehaviour
     {
+        /// <summary>
+        /// This event is used by the PuzzleChecker to update the checklist when a specific puzzle is completed.
+        /// </summary>
         public static event Action<Puzzle> OnSpecificPuzzleCompleted;
+
+        /// <summary>
+        /// This event is used by the PipeRotator to lock the interaction when the puzzle is completed.
+        /// </summary>
         public static event Action OnPuzzleCompleted;
 
         protected void InvokeCompletedEvent()
