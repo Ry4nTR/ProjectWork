4cf5b0611e70f8d890d2889690a92ce9809dac8a
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2025-05-26 20:43:05 +0200

    Included ObjectiveManager into ProjectWork namespace

--- Assets/_Scripts/Objective System/ObjectiveManager.cs
+++ Assets/_Scripts/Objective System/ObjectiveManager.cs
@@ -3,111 +3,113 @@ using System.Collections.Generic;
 using ProjectWork;
 using UnityEngine;
 
-
-public struct ObjectiveDisplayData
+namespace ProjectWork
 {
-    public string Text;
-    public bool IsCompleted;
-}
-
-
-public class ObjectiveManager : MonoBehaviour
-{
-    public static ObjectiveManager Instance { get; private set; }
-
-    public event Action<List<ObjectiveDisplayData>> OnObjectivesUpdated = delegate { };
-
-    public List<CheckListManager<InteractableObject>> activeChecklists = new();
-
-    private void Awake()
+    public struct ObjectiveDisplayData
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            // Ensure this initializes before other scripts
-            DontDestroyOnLoad(gameObject);
-        }
-        else if (Instance != this)
-        {
-            Destroy(gameObject);
-        }
+        public string Text;
+        public bool IsCompleted;
     }
 
-    public void RegisterChecklist(CheckListManager<InteractableObject> checklist)
+
+    public class ObjectiveManager : MonoBehaviour
     {
-        if (!activeChecklists.Contains(checklist))
+        public static ObjectiveManager Instance { get; private set; }
+
+        public event Action<List<ObjectiveDisplayData>> OnObjectivesUpdated = delegate { };
+
+        public List<CheckListManager<InteractableObject>> activeChecklists = new();
+
+        private void Awake()
         {
-            activeChecklists.Add(checklist);
-            checklist.OnListCompleted += HandleChecklistCompletion;
-            UpdateObjectiveDisplay();
-        }
-    }
-
-    public void UnregisterChecklist(CheckListManager<InteractableObject> checklist)
-    {
-        if (activeChecklists.Remove(checklist))
-        {
-            checklist.OnListCompleted -= HandleChecklistCompletion;
-            UpdateObjectiveDisplay();
-        }
-    }
-
-    private void HandleChecklistCompletion()
-    {
-        // Renamed from UpdateObjectives to be more clear
-        UpdateObjectiveDisplay();
-    }
-
-    private void UpdateObjectiveDisplay()
-    {
-        var displayData = new List<ObjectiveDisplayData>();
-
-        foreach (var checklist in activeChecklists)
-        {
-            foreach (var item in checklist.Items)
+            if (Instance == null)
             {
-                var objectiveDef = item.element.objectiveDefinition;
-                if (objectiveDef == null || !objectiveDef.isMandatory) continue;
-
-                if (!(objectiveDef.hideWhenCompleted && item.isCompleted))
-                {
-                    displayData.Add(new ObjectiveDisplayData
-                    {
-                        Text = objectiveDef.displayText,
-                        IsCompleted = item.isCompleted
-                    });
-                }
+                Instance = this;
+                // Ensure this initializes before other scripts
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
             }
         }
 
-        OnObjectivesUpdated?.Invoke(displayData);
-    }
-
-    public List<ObjectiveDisplayData> GetCurrentObjectives()
-    {
-        var currentObjectives = new List<ObjectiveDisplayData>();
-
-        foreach (var checklist in activeChecklists)
+        public void RegisterChecklist(CheckListManager<InteractableObject> checklist)
         {
-            foreach (var item in checklist.Items)
+            if (!activeChecklists.Contains(checklist))
             {
-                if (item.element.objectiveDefinition != null && item.element.objectiveDefinition.isMandatory)
-                {
-                    currentObjectives.Add(new ObjectiveDisplayData
-                    {
-                        Text = item.element.objectiveDefinition.displayText,
-                        IsCompleted = item.isCompleted
-                    });
-                }
+                activeChecklists.Add(checklist);
+                checklist.OnListCompleted += HandleChecklistCompletion;
+                UpdateObjectiveDisplay();
             }
         }
 
-        return currentObjectives;
-    }
+        public void UnregisterChecklist(CheckListManager<InteractableObject> checklist)
+        {
+            if (activeChecklists.Remove(checklist))
+            {
+                checklist.OnListCompleted -= HandleChecklistCompletion;
+                UpdateObjectiveDisplay();
+            }
+        }
 
-    public void ForceUpdateObjectives()
-    {
-        var currentObjectives = GetCurrentObjectives();
-        OnObjectivesUpdated?.Invoke(currentObjectives);
+        private void HandleChecklistCompletion()
+        {
+            // Renamed from UpdateObjectives to be more clear
+            UpdateObjectiveDisplay();
+        }
+
+        private void UpdateObjectiveDisplay()
+        {
+            var displayData = new List<ObjectiveDisplayData>();
+
+            foreach (var checklist in activeChecklists)
+            {
+                foreach (var item in checklist.Items)
+                {
+                    var objectiveDef = item.element.objectiveDefinition;
+                    if (objectiveDef == null || !objectiveDef.isMandatory) continue;
+
+                    if (!(objectiveDef.hideWhenCompleted && item.isCompleted))
+                    {
+                        displayData.Add(new ObjectiveDisplayData
+                        {
+                            Text = objectiveDef.displayText,
+                            IsCompleted = item.isCompleted
+                        });
+                    }
+                }
+            }
+
+            OnObjectivesUpdated?.Invoke(displayData);
+        }
+
+        public List<ObjectiveDisplayData> GetCurrentObjectives()
+        {
+            var currentObjectives = new List<ObjectiveDisplayData>();
+
+            foreach (var checklist in activeChecklists)
+            {
+                foreach (var item in checklist.Items)
+                {
+                    if (item.element.objectiveDefinition != null && item.element.objectiveDefinition.isMandatory)
+                    {
+                        currentObjectives.Add(new ObjectiveDisplayData
+                        {
+                            Text = item.element.objectiveDefinition.displayText,
+                            IsCompleted = item.isCompleted
+                        });
+                    }
+                }
+            }
+
+            return currentObjectives;
+        }
+
+        public void ForceUpdateObjectives()
+        {
+            var currentObjectives = GetCurrentObjectives();
+            OnObjectivesUpdated?.Invoke(currentObjectives);
+        }
     }
 }
