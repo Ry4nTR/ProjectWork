8cd08ab64ee5c0fb3d5b879e9350a02ad9ce6cce
Ry4nTR <rostukterletskyy@gmail.com>
2025-05-29 17:30:10 +0200

    Create the hangar card

--- Assets/_Scripts/Dialogue System/HangarPrisoner.cs
+++ Assets/_Scripts/Dialogue System/HangarPrisoner.cs
@@ -5,19 +5,23 @@ namespace ProjectWork
     [RequireComponent(typeof(Collider))]
     public class HangarPrisoner : InteractableObject
     {
+        [SerializeField] private GameObject objectToSpawn; // Assegna l'oggetto da spawnare nell'Inspector
+        [SerializeField] private Transform spawnPosition; // Posizione dove spawnare l'oggetto
+
         private Collider npcCollider;
+        private bool hasInteracted = false;
 
         private void Awake()
         {
             npcCollider = GetComponent<Collider>();
             DialogueManager.OnDialogueStarted += DisableCollider;
-            DialogueManager.OnDialogueFinished += EnableCollider;
+            DialogueManager.OnDialogueFinished += OnDialogueFinished;
         }
 
         private void OnDestroy()
         {
             DialogueManager.OnDialogueStarted -= DisableCollider;
-            DialogueManager.OnDialogueFinished -= EnableCollider;
+            DialogueManager.OnDialogueFinished -= OnDialogueFinished;
         }
 
         private void EnableCollider() => npcCollider.enabled = true;
@@ -25,7 +29,8 @@ namespace ProjectWork
 
         protected override void InteractChild()
         {
-            // Get the DialogueTrigger component and trigger dialogue
+            if (hasInteracted) return;
+
             var dialogueTrigger = GetComponent<HangarPrisonerDialogueTrigger>();
             if (dialogueTrigger != null)
             {
@@ -34,6 +39,25 @@ namespace ProjectWork
             else
             {
                 Debug.LogError("No HangarPrisonerDialogueTrigger component found!", this);
+            }
+        }
+
+        private void OnDialogueFinished()
+        {
+            if (hasInteracted) return;
+
+            hasInteracted = true;
+            LockInteraction(); // Disabilita ulteriori interazioni
+            DisableCollider(); // Disabilita il collider
+
+            // Spawn dell'oggetto
+            if (objectToSpawn != null && spawnPosition != null)
+            {
+                Instantiate(objectToSpawn, spawnPosition.position, spawnPosition.rotation);
+            }
+            else
+            {
+                Debug.LogWarning("Object to spawn or spawn position not assigned!", this);
             }
         }
     }
